faf2531ae132143d2733d981832bb11e440102bb
BrickheadJohnny <johnny.zgen@gmail.com>
2022-04-27 23:21:33 +0200

    Removed checks temporarily

--- src/pages/api/ipfs/[hash].ts
+++ src/pages/api/ipfs/[hash].ts
@@ -5,14 +5,14 @@ const urlStart = process.env.NODE_ENV === "development" ? "http://" : "https://"
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const hash = req.query.hash?.toString()
 
-  if (
-    !hash ||
-    !req.headers.referrer
-      ?.toString()
-      ?.replace(urlStart, "")
-      ?.startsWith(req.headers.host)
-  )
-    return res.status(404).send("Not found")
+  // if (
+  //   !hash ||
+  //   !req.headers.referrer
+  //     ?.toString()
+  //     ?.replace(urlStart, "")
+  //     ?.startsWith(req.headers.host)
+  // )
+  //   return res.status(404).send("Not found")
 
   const ipfsRes = await fetch(`${process.env.IPFS_GATEWAY}/${hash}`).then((image) =>
     image.blob()

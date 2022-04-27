df3cd026bd18b2f506724416e2e33e67ea8cb8c6
BrickheadJohnny <johnny.zgen@gmail.com>
2022-04-27 23:07:33 +0200

    Endpoint fix + accept reqs only from our app

--- src/pages/api/ipfs/[hash].ts
+++ src/pages/api/ipfs/[hash].ts
@@ -1,17 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-const guildUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : // TODO: replace this with "https://guild.xyz"
-      "https://guild.xyz"
+const urlStart = process.env.NODE_ENV === "development" ? "http://" : "https://"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const hash = req.query.hash?.toString()
-  console.log("Host:", req.headers.host, "Referrer:", req.headers.referer)
 
-  // if (!hash || !req.headers.referrer?.toString()?.startsWith(guildUrl))
-  //   return res.status(404).send("Not found")
+  if (
+    !hash ||
+    !req.headers.referrer
+      ?.toString()
+      ?.replace(urlStart, "")
+      ?.startsWith(req.headers.host)
+  )
+    return res.status(404).send("Not found")
 
   const ipfsRes = await fetch(`${process.env.IPFS_GATEWAY}/${hash}`).then((image) =>
     image.blob()
@@ -24,7 +25,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     "Content-Length": Buffer.byteLength(buffer),
   })
 
-  return res.send(Buffer.from(buffer))
+  res.end(Buffer.from(buffer))
 }
 
 export default handler
